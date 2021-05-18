cf637e70104a85d697f080cbd933ee5273f34c09
Felix Prima <felix.prima@binus.ac.id>
2021-05-18 15:49:09 +0700

    Initial Commit

---
+++ DML_DBProject.sql
@@ -0,0 +1,5 @@
+USE DBProject
+
+exec Input_Customer 'Felix Prima', 'felix.prima@binus.ac.id', 'Binus Square', '2001/07/12','Male', '081188119111', 'Udin123', 'CU001' 
+exec Input_Customer 'Felix Prima', 'felix.prima@binus.ac.id', 'Binus Square', '2001/07/12','Male', '081188119111', 'Udin123', 'CU002' 
+GO
